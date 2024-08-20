37ed38e0477b5f08d8918ad92e8d636773f7626b
Huyen Mi <phamthihuyenmy2002@gmail.com>
2024-08-20 10:14:33 +0700

    fix pandas #1

--- learn.py
+++ learn.py
@@ -1,34 +1,58 @@
 import openpyxl.workbook
+import constant as const
 import pandas as pd
 import numpy  as np
 import openpyxl
 from openpyxl.styles import PatternFill
 
-rows, cols = 100, 100
-data=np.random.randint(-100,100,size=(rows,cols))
-df=pd.DataFrame(data)
-df.to_excel('random_data.xlsx',index=False)
 
-df['Row_mean'] = df.mean(axis=1)
-df.loc['Col_mean'] = df.mean(axis=0)
 
-df.to_excel('mean_data.xlsx', index=False)
+def create_random_data(rows, cols, output_excel):
+    data=np.random.randint(-100,100,size=(rows,cols))
+    df=pd.DataFrame(data)
+    df.to_excel(output_excel,index=False)
+    return df
 
-wb=openpyxl.load_workbook('mean_data.xlsx')
-ws=wb.active
+def calculate_mean(df,output_excel):
+    df['Row_mean'] = df.mean(axis=1)
+    df.loc['Col_mean'] = df.mean(axis=0)
+    df.to_excel(output_excel, index=False)
+    return output_excel
 
-for row in range(2,rows+2):
-    cell=ws.cell(row=row, column=cols+1)
+
+def apply_color_base_value(cell, negative=const.RED_COLOR, positive=const.GREEN_COLOR):
     if cell.value <0:
-        cell.fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
-    else:
-        cell.fill = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+        cell.fill=PatternFill(start_color=negative, end_color=negative,fill_type="solid")
+    else :
+        cell.fill=PatternFill(start_color=positive, end_color=positive,fill_type="solid")
 
-for col in range(1, cols+2):
-    cell=ws.cell(row=cols+2, column=col)
-    if cell.value <0:
-       cell.fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
-    else:
-        cell.fill = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
 
-wb.save('mean_data_colored.xlsx')
+def color_mean(file_path,output_excel,rows,cols):
+    wb=openpyxl.load_workbook(file_path)
+    ws=wb.active
+
+    start_row = 2
+    end_row = rows+2
+
+    start_col = 1
+    end_col = cols+1
+
+    for row in range(start_row, end_row):
+        cell=ws.cell(row=row, column=end_col)
+        apply_color_base_value(cell)
+
+    for col in range(start_col, end_col):
+        cell=ws.cell(row=end_row, column=col)
+        apply_color_base_value(cell)
+
+    wb.save(output_excel)
+
+def main():
+    rows= const.ROWS
+    cols= const.COLS
+    df=create_random_data(rows, cols,'random_data.xlsx')
+    file_path=calculate_mean(df,'mean_data.xlsx')
+    color_mean(file_path,'mean_data_colored.xlsx',rows,cols)
+
+if __name__ == "__main__":
+    main()
