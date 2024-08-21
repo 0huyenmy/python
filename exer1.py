cbe97e261a77e4566134ade7ddbfd5fdc1ebe5d9
Titus <trinhchuongtran@gmail.com>
2024-08-21 23:42:42 +0700

    exer: 1, 2

---
+++ exer1.py
@@ -0,0 +1,2 @@
+# Exercise 1
+# Lọc ra các từ có ít nhất 2 nguyên âm (EUOAI) từ sheet `Excercise1` trong file `ueoai.xlsx` và ghi vào sheet `Filtered` trong cùng file.
---
+++ exer2.py
@@ -0,0 +1,4 @@
+# Exercise 1
+# Lọc ra cac valid email từ sheet `Email` trong file `email.xlsx` và in ra.
+# Với
+pattern ='[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}'
